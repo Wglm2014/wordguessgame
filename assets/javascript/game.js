0b5b1a7487883b75265e9c1af8453672d36ae43d
wglm2014 <glinaresmiranda@yahoo.com>
2019-04-03 22:31:19 -0400

    progress, functionality added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,62 @@
 //charges all the elements from the dom
-window.onload = function () {
-    var lettersPress = document.getElementById("lettersPress");
-    var guessedWord = document.getElementById("guessedWord");
-    var categoriesList = document.getElementById("categoriesList");
+var lettersPress = document.getElementById("lettersPress");
+var guessedWord = document.getElementById("guessedWord");
+var categoriesList = document.getElementById("categoriesList");
+var hint = document.getElementById("hint");
 
+//Global variables
+var totalWordLetters = 0;
+var words = [];
+var hints = [];
+var word = "";
 
-    //Global variables
-    var totalWordLetters = 0;
-    var words = [];
-    var hints = [];
-    var word = "_";
-
-    //opens the file with the words and hints
-    function categorieSelected(selection) {
-        var xhttp;
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var file = JSON.parse(this.responseText);
-            }
-        };
-        xhttp.open("GET", "./javascript/" + categoriesList.nodeValue + ".txt", true);
-        xhttp.send();
-
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+document.onkeyup = function (event) {
+    if (totalWordLetters === word.length) return;
+    var userKey = event.key;
+    //only is the user press and alphabet letter
+    if (alphabet.includes(userKey)) {
+        totalWordLetters++;
+        lettersPress.textContent += userKey;
     }
-
-
-
-    //fill the arrays for the words and the hints
-
-
-    //
-    document.onkeyup = function (event) {
-        var userKey = event.key;
-        //only is the user press and alphabet letter
-        if (alpahbet.contains(userKey)) {
-            totalWordLetters++;
-            lettersPress.textContent += userKey;
-        }
-    }
-
-    function play() {
-        word.forEach(element => {
-            guessedWord.textContent += "-";
-        });
-
-        if (totalWordLetters === word.length) return;
-    }
-
 }
+
+function play() {
+    var number = Math.floor(Math.random() * words.length);
+    word = words[number][0];
+    hints = words[number][1];
+    console.log(word);
+    var wordArray = word.split("");
+    console.log(wordArray.length);
+    wordArray.forEach(element => {
+        guessedWord.textContent += "-";
+    });
+
+    hint.textContent = hints[Math.floor(Math.random() * hints.length)];
+
+    if (totalWordLetters === word.length) return;
+}
+
+//opens the file with the words and hints
+function categorySelected(selection) {
+    words = categories[selection];
+    console.log(words)
+}
+
+
+var categories = [
+    [
+        ["batman", ["lives in Gotham city", "he is the dark night", "secretly guys want to be like him"]],
+        ["superman", ["he is a reporter", "the yellow light from the sun make him powerful", "he is the man of iron"]],
+        ["wonder woman", ["she is an amazon", "her name is Diana", "she is a feminist"]]
+    ],
+    [
+        ["the joker", ["his face has a creepy smile", "he has very dark humor", "his girlfriend is a harlequin"]],
+        ["lex luthor", ["he hates superman", "is the ceo of his own Company", "IQ level of 17"]]
+
+    ]];
+
+
+
+
 
